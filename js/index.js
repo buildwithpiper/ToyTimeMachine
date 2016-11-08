01c301de3ac00d7599cfd0508721837738f91efe
Daniel Gillies <danny@skully.com>
2016-11-07 18:03:22 -0800

    Fix text box submission

--- js/index.js
+++ js/index.js
@@ -13,7 +13,14 @@ $(".content").click(function(event) {
     nextToy();
 });
 $(".next-toy").click(function(event) {
-	nextToy();
+    nextToy();
+});
+
+/** Enter key submits **/
+$("#year").keyup(function(event){
+    if(event.keyCode == 13){
+        $(".gobtn").click();
+    }
 });
 
 /** Choose correct decade based on year of birth **/
@@ -22,20 +29,28 @@ $(".gobtn").click(function(event) {
     var year = $("#year").val();
     if (year < 1950) {
         $("#tab-1").prop("checked", true);
+        changeContent(0);
     } else if (year < 1960) {
         $("#tab-2").prop("checked", true);
+        changeContent(1);
     } else if (year < 1970) {
         $("#tab-3").prop("checked", true);
+        changeContent(2);
     } else if (year < 1980) {
         $("#tab-4").prop("checked", true);
+        changeContent(3);
     } else if (year < 1990) {
         $("#tab-5").prop("checked", true);
+        changeContent(4);
     } else if (year < 2000) {
         $("#tab-6").prop("checked", true);
+        changeContent(5);
     } else if (year < 2010) {
         $("#tab-7").prop("checked", true);
+        changeContent(6);
     } else {
         $("#tab-8").prop("checked", true);
+        changeContent(7);
     }
 });
 
@@ -60,35 +75,35 @@ function changeContent(tab) {
     currentToy = 0;
     switch (tab) {
         case "tab-1":
-        	decade = 0;
-        	nextToy();
+            decade = 0;
+            nextToy();
             break;
         case "tab-2":
-        	decade = 1;
-        	nextToy();
+            decade = 1;
+            nextToy();
             break;
         case "tab-3":
-        	decade = 2;
-        	nextToy();
+            decade = 2;
+            nextToy();
             break;
         case "tab-4":
-        	decade = 3;
-        	nextToy();
+            decade = 3;
+            nextToy();
             break;
         case "tab-5":
-        	decade = 4;
+            decade = 4;
             nextToy();
             break;
         case "tab-6":
-        	decade = 5;
-        	nextToy();
+            decade = 5;
+            nextToy();
             break;
         case "tab-7":
-        	decade = 6;
-        	nextToy();
+            decade = 6;
+            nextToy();
             break;
         case "tab-8":
-        	decade = 7;
+            decade = 7;
             $("#buy").css("display", "inline-block");
             nextToy();
             break;
@@ -96,17 +111,17 @@ function changeContent(tab) {
 }
 
 function nextToy() {
-	// if (contentArray[currentDecade].length == 1)
-	// 	$(".side-arrow").hide();
-	if (toy >= content[decade].length)
-		toy = 0;
-	console.log("DECADE: " + decade + " TOY: " + toy);
+    // if (contentArray[currentDecade].length == 1)
+    //  $(".side-arrow").hide();
+    if (toy >= content[decade].length)
+        toy = 0;
+    console.log("DECADE: " + decade + " TOY: " + toy);
     $(".content .title").html(content[decade][toy].title);
     $(".content .current-year").html(content[decade][toy].year);
     $(".content .share").html(content[decade][toy].share);
     $(".video-overlay").css("background-color", content[decade][toy].color);
     $("#ytplayer").prop("src", "https://www.youtube.com/embed/" + content[decade][toy].video + "?playlist=" + content[decade][toy].video + 
-    	"&rel=0&enablejsapi=1&autoplay=1&controls=0&showinfo=0&loop=1&iv_load_policy=3")
+        "&rel=0&enablejsapi=1&autoplay=1&controls=0&showinfo=0&loop=1&iv_load_policy=3")
     toy++;
 }
 
