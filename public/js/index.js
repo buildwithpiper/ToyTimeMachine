081d90c6855055eb4e0a564d0bd3b22ca311c65a
Greg Guterman <greg@playpiper.com>
2016-11-23 14:04:51 -0800

    remove 2010, sidebar slide on mobile

--- public/js/index.js
+++ public/js/index.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
     });
 });
 
-var imagePaths = {1940: [], 1950: [], 1960: [], 1970: [], 1980: [], 1990: [], 2000: [], 2010: []};
+var imagePaths = {1940: [], 1950: [], 1960: [], 1970: [], 1980: [], 1990: [], 2000: []};
 var loadCount = 0;
 var currentDecade = 0;
 var imagesLoading = 0;
